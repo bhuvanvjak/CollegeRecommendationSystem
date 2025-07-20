4090f6eaa13e4728a881f2828ac120f14e898170
bhuvanvjak <bhuvanesh9139@gmail.com>
2025-07-20 19:33:19 +0530

    Made changes for deployment

--- main_app.py
+++ main_app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from usapp import app as usa_bp
 from canadaap import app as canada_bp
@@ -17,4 +18,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
